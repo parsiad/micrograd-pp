dff937154edf08133717a6dad125f57e38bc3182
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-08-18 17:38:23 -0400

    Add MultiheadAttention

--- src/micrograd_pp/__init__.py
+++ src/micrograd_pp/__init__.py
@@ -1,6 +1,6 @@
 from ._expr import Constant, Expr, Parameter, is_grad_enabled, maximum, no_grad, relu
-from ._func import cross_entropy_loss
-from ._nn import BatchNorm1d, Dropout, Embedding, Linear, ReLU, Sequential, eval, is_eval
+from ._func import cat, cross_entropy_loss, softmax
+from ._nn import BatchNorm1d, Dropout, Embedding, Linear, Module, MultiheadAttention, ReLU, Sequential, eval, is_eval
 from ._opt import SGD
 
 from . import datasets
@@ -12,10 +12,13 @@ __all__ = (
     "Embedding",
     "Expr",
     "Linear",
+    "Module",
+    "MultiheadAttention",
     "Parameter",
     "ReLU",
     "Sequential",
     "SGD",
+    "cat",
     "cross_entropy_loss",
     "datasets",
     "eval",
@@ -24,4 +27,5 @@ __all__ = (
     "maximum",
     "no_grad",
     "relu",
+    "softmax",
 )
--- src/micrograd_pp/_func.py
+++ src/micrograd_pp/_func.py
@@ -1,7 +1,47 @@
+from typing import Sequence
+
 import numpy as np
 import numpy.typing as npt
 
 from ._expr import Expr
+
+
+class _Cat(Expr):
+    def __init__(self, tensors: Sequence[Expr], dim: int) -> None:
+        self._dim = dim
+        self._tensors = tensors
+        super().__init__(value=np.concatenate([tensor._value for tensor in tensors], axis=dim), children=tensors)
+
+    def _backward(self, grad: npt.NDArray) -> None:
+        lo = 0
+        for tensor in self._tensors:
+            hi = lo + tensor.shape[self._dim]
+            tensor.update_grad(lambda lo_=lo, hi_=hi: grad.take(indices=np.arange(lo_, hi_), axis=self._dim))
+            lo = hi
+
+
+def _log_sum_exp(input_: Expr, dim: int) -> tuple[Expr, Expr]:
+    input_max = input_.max(dim=dim, keepdim=True)
+    delta = input_ - input_max
+    log_sum_exp = delta.exp().sum(dim=dim, keepdim=True).log()
+    return delta, log_sum_exp
+
+
+def cat(tensors: Sequence[Expr], dim: int = 0) -> Expr:
+    """Concatenates a sequence of tensors along a given dimension.
+
+    Parameters
+    ----------
+    tensors
+        Input tensors
+    dim
+        Dimension along which to concatenate
+
+    Returns
+    -------
+    Concatenated tensor
+    """
+    return _Cat(tensors=tensors, dim=dim)
 
 
 def cross_entropy_loss(input_: Expr, target: npt.NDArray) -> Expr:
@@ -19,7 +59,23 @@ def cross_entropy_loss(input_: Expr, target: npt.NDArray) -> Expr:
     Cross entropy loss
     """
     n, _ = input_.shape
-    input_max = input_.max(dim=1, keepdim=True)
-    delta = input_ - input_max
-    log_sum_exp = delta.exp().sum(dim=1).log().squeeze()
-    return (log_sum_exp - delta[np.arange(n), target]).sum() / n
+    delta, log_sum_exp = _log_sum_exp(input_=input_, dim=1)
+    return (log_sum_exp.squeeze() - delta[np.arange(n), target]).mean()
+
+
+def softmax(input_: Expr, dim: int) -> Expr:
+    """Computes the softmax.
+
+    Parameters
+    ----------
+    input_
+        Logits
+    dim
+        Dimension along which to compute softmax
+
+    Returns
+    -------
+    Softmax
+    """
+    delta, log_sum_exp = _log_sum_exp(input_=input_, dim=dim)
+    return (delta - log_sum_exp).exp()
