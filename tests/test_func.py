90fe7c08086b65d3acfcac70054ff2c1b5a7fd67
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-08-23 15:00:20 -0400

    Skip test if import missing

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,10 +1,13 @@
 import numpy as np
-import scipy.special
+import pytest
 
 import micrograd_pp as mpp
 
 
+@pytest.mark.skipif(not pytest.importorskip("scipy.special"), reason="Unable to import scipy.special")
 def test_softmax() -> None:
+    import scipy.special
+
     a = np.random.randn(5, 4, 3)
     actual = mpp.softmax(mpp.Constant(a), dim=1).value
     desired = scipy.special.softmax(a, axis=1)
